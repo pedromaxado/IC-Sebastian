508d6c91a8fbc1aa0a40d21a596fa3750253c24a
Pedro Otávio <p.o.maxado@gmail.com>
2017-08-20 22:55:29 -0300

    fixed makefile and started doing the testing routine

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -1,10 +1,15 @@
-#include <bits/stdc++.h>
+#include <iostream>
 #include "../include/Data.h"
+#include "../include/Heap.h"
 #include "../include/Alg1.h"
 #include "../include/Alg2.h"
 #include "../include/AlgT.h"
 #include "../include/AlgQT.h"
 #include "../include/AlgHeap.h"
+
+#define MAXN 10000
+#define PROB 10
+#define REP 30
 
 using namespace std;
 
@@ -14,37 +19,108 @@ double getElapsedTime( clock_t init, clock_t end ) {
 
 int main( int argc, char const *argv[] ) {
 
-    int best;
-	Data data = new_data();
-    pair< int, int > res;
-    res = alg1	( getVecX(data),
-    			  getVecY(data),
-    			  getCompatibilityMtx(data),
-    			  getSize(data),
-    			  &best );
+    FILE* fp;
+    clock_t start, end;
+    double sumTriv, sumATriv, sumAlg1, sumAlg2;
 
-    printf("%d %d", res.first, res.second);
+    fp = fopen("../data/stats.dat", "w");
 
-    res = alg2	( getVecX(data),
-				  getVecY(data),
-				  getCompatibilityMtx(data),
-				  getSize(data) );
+    if ( fp == NULL ) {
+        printf("Erro na abertura de arquivo\n");
+        exit(EXIT_FAILURE);
+    }
 
-    printf("%d %d", res.first, res.second);
+    pair<int,int> res;
 
-    res = TrivialAlg ( getVecX(data),
-            		   getVecY(data),
-            		   getCompatibilityMtx(data),
-            		   getSize(data) );
+    for ( int n = 10; n <= MAXN; n *= 10 ) {
+        sumTriv = sumATriv = sumAlg1 = sumAlg2 = 0;
+        for ( int j = 0; j < REP; j++ ) {
+            srand(1);
 
-    printf("%d %d", res.first, res.second);
+            start = clock();
+            TrivialAlg_Prob(n, getVecX(data), getVecY(data), PROB);
+            end = clock();
 
-    res = AlmostTrivialAlg	( getVecX(data),
-            				  getVecY(data),
-            				  getCompatibilityMtx(data),
-            				  getSize(data) );
+            sumTriv += getElapsedTime(start, end);
 
-    printf("%d %d", res.first, res.second);
+            srand(1);
+
+            start = clock();
+            AlmostTrivialAlg_Prob(n, getVecX(data), getVecY(data), PROB);
+            end = clock();
+
+            sumATriv += getElapsedTime(start, end);
+
+            srand(1);
+
+            start = clock();
+            alg1_Prob(n, getVecX(data), getVecY(data), PROB);
+            end = clock();
+
+            sumAlg1 += getElapsedTime(start, end);
+
+            srand(1);
+
+            start = clock();
+            alg2_Prob(n, getVecX(data), getVecY(data), PROB);
+            end = clock();
+
+            sumAlg2 += getElapsedTime(start, end);
+        }
+
+        sumTriv /= REP;
+        sumATriv /= REP;
+        sumAlg1 /= REP;
+        sumAlg2 /= REP;
+
+        fprintf(fp, "Trivial n = %d\n", n);
+        fprintf(fp, "%lf\n\n", sumTriv);
+
+        fprintf(fp, "Quase Trivial n = %d\n", n);
+        fprintf(fp, "%lf\n\n", sumATriv);
+
+        fprintf(fp, "Alg 1 n = %d\n", n);
+        fprintf(fp, "%lf\n\n", sumAlg1);
+
+        fprintf(fp, "Alg 2 n = %d\n", n);
+        fprintf(fp, "%lf\n\n", sumAlg2);
+    }
+
+    fclose(fp);
+
+    // int best;
+	// Data data = new_data();
+    // pair< int, int > res;
+    // res = alg1	( getVecX(data),
+    // 			  getVecY(data),
+    // 			  getCompatibilityMtx(data),
+    // 			  getSize(data),
+    // 			  &best );
+    //
+    // printf("%d %d\n", res.first, res.second);
+    //
+    // res = alg2	( getVecX(data),
+	// 			  getVecY(data),
+	// 			  getCompatibilityMtx(data),
+	// 			  getSize(data) );
+    //
+    // printf("%d %d\n", res.first, res.second);
+    //
+    // res = TrivialAlg ( getVecX(data),
+    //         		   getVecY(data),
+    //         		   getCompatibilityMtx(data),
+    //         		   getSize(data) );
+    //
+    // printf("%d %d\n", res.first, res.second);
+    //
+    // res = AlmostTrivialAlg	( getVecX(data),
+    //         				  getVecY(data),
+    //         				  getCompatibilityMtx(data),
+    //         				  getSize(data) );
+    //
+    // printf("%d %d\n", res.first, res.second);
+    //
+    // destroy_data(data);
 
     return 0;
 }
