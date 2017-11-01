1e1e2fbaebdd01dd4a5a6ab5ffd6b43d3fe07422
Pedro Otávio <p.o.maxado@gmail.com>
2017-11-01 00:12:57 -0200

    this is so crappy

--- code/scripts/clean_data.sh
+++ code/scripts/clean_data.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-for file in data/*.csv
+for file in data/all/1000_rep/seed_*/*.csv
+do
+    cat /dev/null > $file
+done
+
+for file in data/triv_vs_atriv/seed_*/*.csv
 do
     cat /dev/null > $file
 done
